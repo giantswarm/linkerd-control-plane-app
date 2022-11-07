816e70e4fd938ef1bd2318be1a1497570d6f447f
Matias Charriere <matias@giantswarm.io>
2022-11-07 16:29:29 +0100

    Reduce hardcoded deps in Makefile.
    For that, I moved some actions directly to vendir. Added also a looping
    mechanism over dependencies to automatically update the Chart.yaml

--- Makefile.local.mk
+++ Makefile.local.mk
@@ -1,31 +1,23 @@
 ##@ Chart
 
+YQ=docker run --rm -u $$(id -u) -v $${PWD}:/workdir mikefarah/yq
+HELM_DOCS=docker run --rm -u $$(id -u) -v $${PWD}:/helm-docs jnorwood/helm-docs:latest
+
 MAIN_CHART=helm/linkerd-control-plane
-VENDOR_CHART_LINKERD=vendor/linkerd/charts/linkerd-control-plane
-VENDOR_CHART_PARTIALS=vendor/linkerd/charts/partials
-VENDOR_CHART_CRDS=vendor/linkerd/charts/linkerd-crds
+DEPS := $(shell ls $(MAIN_CHART)/charts)
 
-
-.PHONY: update-chart helm-docs apply-vendor update-deps
+.PHONY: update-chart helm-docs update-deps $(DEPS)
 update-chart: ## Run vendir sync
 	@echo "====> $@"
 	vendir sync
-	$(MAKE) apply-vendor
 	$(MAKE) update-deps
 
-update-deps:
-	new_version=`docker run --rm -u $$(id -u) -v "$${PWD}":/workdir mikefarah/yq .version $(MAIN_CHART)/charts/linkerd-crds/Chart.yaml` && \
-	docker run --rm -u $$(id -u) -v "$${PWD}":/workdir mikefarah/yq -i e "with(.dependencies[]; select(.name == \"linkerd-crds\") | .version = \"$$new_version\")" $(MAIN_CHART)/Chart.yaml
+update-deps: $(DEPS)
 	cd $(MAIN_CHART) && helm dependency update
 
-helm-docs:
-	docker run --rm --volume "`pwd`:/helm-docs" -u `id -u` jnorwood/helm-docs:latest -c $(MAIN_CHART) -g $(MAIN_CHART)
+$(DEPS):
+	new_version=`$(YQ) .version $(MAIN_CHART)/charts/$@/Chart.yaml` && \
+	$(YQ) -i e "with(.dependencies[]; select(.name == \"$@\") | .version = \"$$new_version\")" $(MAIN_CHART)/Chart.yaml
 
-apply-vendor:
-	mkdir -p $(MAIN_CHART)/charts
-	rm -rf $(MAIN_CHART)/templates
-	rm -rf $(MAIN_CHART)/charts/partials
-	rm -rf $(MAIN_CHART)/charts/linkerd-crds
-	cp -r $(VENDOR_CHART_LINKERD)/templates $(MAIN_CHART)
-	cp -r $(VENDOR_CHART_PARTIALS) $(MAIN_CHART)/charts/
-	cp -r $(VENDOR_CHART_CRDS) $(MAIN_CHART)/charts/
+helm-docs:
+	$(HELM_DOCS) -c $(MAIN_CHART) -g $(MAIN_CHART)
